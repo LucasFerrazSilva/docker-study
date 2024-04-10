2b8d9855789c2c4839d105deac15c36194cad751
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2024-04-10 06:58:29 -0300

    add files

---
+++ src/test/java/com/ferraz/dockerstudy/DockerStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferraz.dockerstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
